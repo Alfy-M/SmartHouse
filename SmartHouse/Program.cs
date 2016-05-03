e911cb1b2a1a0191f9e509831aa67a886c05bcf5
AleksPolinkins <a.polinkins@gmail.com>
2016-05-03 18:42:57 +0200

    connection screen  added

--- SmartHouse/Program.cs
+++ SmartHouse/Program.cs
@@ -12,6 +12,7 @@ using System.IO;
 
 
 
+
 using Gadgeteer.Networking;
 using GT = Gadgeteer;
 using GTM = Gadgeteer.Modules;
@@ -24,93 +25,99 @@ namespace SmartHouse
     {
         //double factor;
         private Text txtSerial;
-        private Text txtblank;
-  
+        private Rectangle button_gas;//UIElement type
         private Canvas canvas;
       
         private Font baseFont;
+        private Boolean connection;//true=wifi,false=RJ45;
+        private int window_id;//current menu:0-connection,1-main
+        private Boolean connected;
        
        
         
         // This method is run when the mainboard is powered up or reset.   
         void ProgramStarted()
         {
-            /*******************************************************************************************
-            Modules added in the Program.gadgeteer designer view are used by typing 
-            their name followed by a period, e.g.  button.  or  camera.
-            
-            Many modules generate useful events. Type +=<tab><tab> to add a handler to an event, e.g.:
-                button.ButtonPressed +=<tab><tab>
-            
-            If you want to do something periodically, use a GT.Timer and handle its Tick event, e.g.:
-                GT.Timer timer = new GT.Timer(1000); // every second (1000ms)
-                timer.Tick +=<tab><tab>
-                timer.Start();
-            *******************************************************************************************/
+           
+           
             //factor = gasCalibration();
 
-            //define canvas
-           
-           
+
+            baseFont = Resources.GetFont(Resources.FontResources.NinaB);//font fixed for all
+           //define touch screen click handlar
             displayT35.WPFWindow.TouchDown += new Microsoft.SPOT.Input.TouchEventHandler(screen_click);
-            gasSense.HeatingElementEnabled = true;
-          
-           // displayT35.SimpleGraphics.Clear();
-           // displayT35.SimpleGraphics.BackgroundColor = GT.Color.Purple;
-            //var bc = new BrushConverter();
-            
-            //show_states_page();
-           GT.Timer timer = new GT.Timer(1000);
-          timer.Tick += my_display;
-           timer.Start();
+            connected = false;
+            window_id = 0;
 
-          // display_state_page();
-          
-            /*
+            GT.Timer timer = new GT.Timer(1000);
+            timer.Tick += my_display_managment;
+            timer.Start();
 
-           TempHumidSI70.Measurement temp = tempHumidSI70.TakeMeasurement();
-           //Debug.Print("Temp: " + temp.Temperature.ToString());
-           //Debug.Print("Umidity: " + temp.RelativeHumidity.ToString());
-           gasSense.HeatingElementEnabled = true;
-           double gas = gasSense.ReadProportion();
-           String textt = "Temp: " + temp.Temperature.ToString("F2");
-           String textu = "Umidity: " + temp.RelativeHumidity.ToString("F2");
-           String textg = "Gas: " + gas.ToString("F2");
-           Font mfont = Resources.GetFont(Resources.FontResources.NinaB);
-        
-           displayT35.SimpleGraphics.DisplayText(textt, mfont, Color.White, 10, 10);
-           displayT35.SimpleGraphics.DisplayText(textu, mfont, Color.White, 10, 25);
-           displayT35.SimpleGraphics.DisplayText(textg, mfont, Color.White, 10, 40);
-            */
+     
+
             // Use Debug.Print to show messages in Visual Studio's "Output" window during debugging.*/
             Debug.Print("Program Started");
+        }
+
+       void my_display_managment(GT.Timer timer){
+           if (connected)
+           {
+               if (window_id == 1) { my_display_main(); }
+           }
+           else {
+               my_connection_menu();
+           }
+        
         }
 
         void screen_click(object sender, Microsoft.SPOT.Input.TouchEventArgs e) {
             int x;
             int y;
             e.GetPosition(displayT35.WPFWindow, 0, out x, out y);
-            if (x >= 259 && x < 340 && y >= 199 && y < 240) {
-                //gas on of button pressed
-                if (gasSense.HeatingElementEnabled)
+            if (window_id == 1)
+            {//se pressed on main menu
+                if (x >= 259 && x < 340 && y >= 199 && y < 240)
                 {
-                    gasSense.HeatingElementEnabled = false;
+                    //gas on of button pressed
+                    if (gasSense.HeatingElementEnabled)
+                    {
+                        gasSense.HeatingElementEnabled = false;
+
+                    }
+                    else
+                    {
+                        gasSense.HeatingElementEnabled = true;
+                    }
 
                 }
-                else {
-                    gasSense.HeatingElementEnabled = true;
-                }
-               
+            }
+            if (window_id == 0) {
+                if ((y >= 60 && y < 120) ||( y >= 160 && y < 220))
+                {
+                    if (y >= 60 && y < 120)
+                    {//wifi chosen
+                        connection = true;
+                    }
+                    if (y >= 160 && y < 220)
+                    {//rj45 chosen
+                        connection = false;
+                    }
+                    //pressed on connecton menu
+                    connected = true;
+                    window_id = 1;//pass to next menu
+                    gasSense.HeatingElementEnabled = true;//turn on gas sensor
+                    //TODO: manage connections
+                }//else do nothing
             }
         }
         
-        void my_display(GT.Timer timer)
-
-
+        //void my_display_main(GT.Timer timer)
+        void my_display_main()
         {
 
             baseFont = Resources.GetFont(Resources.FontResources.NinaB);
             canvas = new Canvas();
+            displayT35.WPFWindow.Background = new SolidColorBrush(GT.Color.Cyan);
             displayT35.WPFWindow.Child = canvas;
             TempHumidSI70.Measurement temp = tempHumidSI70.TakeMeasurement();
 
@@ -118,33 +125,31 @@ namespace SmartHouse
             String textt = temp.Temperature.ToString("F2");
             String textu = temp.RelativeHumidity.ToString("F2");
             String textg = gas.ToString("F2")+" (V: " +gasSense.ReadVoltage().ToString("F2")+" )";
-            
-            //var window = displayT35.WPFWindow;
+                  
             baseFont = Resources.GetFont(Resources.FontResources.NinaB);
-            //canvas_dyn = new Canvas();
-            //window.Child = canvas_dyn;
+      
 
-
-
+            //text
             txtSerial = new Text(baseFont, "Temp: ");
+            txtSerial.ForeColor = GT.Color.Purple;
             canvas.Children.Add(txtSerial);
             Canvas.SetTop(txtSerial, 40);
             Canvas.SetLeft(txtSerial, 60);
 
-            // The following displays as: The serial device is...
-            //
+         
             txtSerial = new Text(baseFont, "Humidity: ");
+            txtSerial.ForeColor = GT.Color.Purple;
             canvas.Children.Add(txtSerial);
             Canvas.SetTop(txtSerial, 60);
             Canvas.SetLeft(txtSerial, 60);
 
-            // The following displays as: The serial...
-            //
+   
             txtSerial = new Text(baseFont, "Gas: ");
+            txtSerial.ForeColor = GT.Color.Purple;
             canvas.Children.Add(txtSerial);
             Canvas.SetTop(txtSerial, 80);
             Canvas.SetLeft(txtSerial, 60);
-
+            //data from sensors
             txtSerial = new Text(baseFont, textt);
             canvas.Children.Add(txtSerial);
             Canvas.SetTop(txtSerial, 40);
@@ -160,59 +165,112 @@ namespace SmartHouse
             Canvas.SetTop(txtSerial, 80);
             Canvas.SetLeft(txtSerial, 150);
 
+            //connection type
+                //draw frame
+            Rectangle button_gas = new Rectangle(154, 40);
+            button_gas.Stroke = new Pen(GT.Color.Purple);//colore di bordo
+            canvas.Children.Add(button_gas);
+            Canvas.SetTop(button_gas, 140);
+            Canvas.SetLeft(button_gas, 165);
 
-            /*
-           
+            txtSerial = new Text(baseFont, "Connection type: ");
+            canvas.Children.Add(txtSerial);
+            Canvas.SetTop(txtSerial, 150);
+            Canvas.SetLeft(txtSerial, 170);
+
+            if (connection) {
+                txtSerial = new Text(baseFont, "WiFi");
+                canvas.Children.Add(txtSerial);
+                Canvas.SetTop(txtSerial, 150);
+                Canvas.SetLeft(txtSerial, 285);
+
+            } else {
+                txtSerial = new Text(baseFont, "RJ45");
+                canvas.Children.Add(txtSerial);
+                Canvas.SetTop(txtSerial, 150);
+                Canvas.SetLeft(txtSerial, 285);
+
+            
+            }
+
+            //gas on/of
+            txtSerial = new Text(baseFont, "Gas Sensor: ");
+            canvas.Children.Add(txtSerial);
+            Canvas.SetTop(txtSerial, 213);
+            Canvas.SetLeft(txtSerial,170);
+
+            button_gas = new Rectangle(100,60);
+            button_gas.Stroke = new Pen(GT.Color.Purple);//colore di bordo
+            canvas.Children.Add(button_gas);
+            Canvas.SetTop(button_gas, 199);
+            Canvas.SetLeft(button_gas, 259);
+    
+           //manage gas button state
             if (gasSense.HeatingElementEnabled)
             {
-                displayT35.SimpleGraphics.DisplayText("ON", mfont, GT.Color.Red, 284, 213);
+                
+                txtSerial = new Text(baseFont, "ON");
+                canvas.Children.Add(txtSerial);
+                Canvas.SetTop(txtSerial,213);
+                Canvas.SetLeft(txtSerial, 280);
+                button_gas.Fill = new SolidColorBrush(GT.Color.Green);
             }
             else {
-                displayT35.SimpleGraphics.DisplayText("OFF", mfont, GT.Color.Red, 284, 213);
+               
+                txtSerial = new Text(baseFont, "OFF");
+                canvas.Children.Add(txtSerial);
+                Canvas.SetTop(txtSerial, 213);
+                Canvas.SetLeft(txtSerial, 280);
+                button_gas.Fill = new SolidColorBrush(GT.Color.Red);
             }
-            */
+            
             
             //Debug.Print("Gas: " + gas);
             
 
         }
 
-        void show_states_page() {
-            Font mfont = Resources.GetFont(Resources.FontResources.NinaB);
-           displayT35.WPFWindow.DisplayModule.SimpleGraphics.DisplayText("Temp: ", mfont, GT.Color.Cyan, 10, 10);
-           displayT35.WPFWindow.DisplayModule.SimpleGraphics.DisplayText("Umidity: ", mfont, GT.Color.Cyan, 10, 25);
-           displayT35.WPFWindow.DisplayModule.SimpleGraphics.DisplayText("Gas: ", mfont, GT.Color.Cyan, 10, 40);
-           displayT35.WPFWindow.DisplayModule.SimpleGraphics.DisplayRectangle(GT.Color.Black, 1, GT.Color.Blue, 259, 199, 60, 40);
-        
-        }
-        private void display_state_page()
-        {
-            
+        void my_connection_menu() {
            
+            canvas = new Canvas();
+            displayT35.WPFWindow.Child = canvas;
+            displayT35.WPFWindow.Background = new SolidColorBrush(GT.Color.Blue);
 
-            //
-            // The following displays as intended: (OK)
-            //
-            txtSerial = new Text(baseFont, "Temp: ");
+            txtSerial = new Text(baseFont, "Choose connection type");
+            txtSerial.ForeColor=GT.Color.Red;
             canvas.Children.Add(txtSerial);
-            Canvas.SetTop(txtSerial, 40);
-            Canvas.SetLeft(txtSerial, 60);
+            Canvas.SetTop(txtSerial,20);
+            Canvas.SetLeft(txtSerial, 80);
 
-            // The following displays as: The serial device is...
-            //
-            txtSerial = new Text(baseFont, "Humidity: ");
-            canvas.Children.Add(txtSerial);
-            Canvas.SetTop(txtSerial, 60);
-            Canvas.SetLeft(txtSerial, 60);
+            Rectangle button_wifi = new Rectangle(320, 60);
+            button_wifi.Fill = new SolidColorBrush(GT.Color.Green); ;//colore di bordo
+            canvas.Children.Add(button_wifi);
+            Canvas.SetTop(button_wifi, 60);
+            Canvas.SetLeft(button_wifi, 0);
 
-            // The following displays as: The serial...
-            //
-            txtSerial = new Text(baseFont, "Gas: ");
+            txtSerial = new Text(baseFont, "WiFi");
+            txtSerial.ForeColor = GT.Color.White;
             canvas.Children.Add(txtSerial);
             Canvas.SetTop(txtSerial, 80);
-            Canvas.SetLeft(txtSerial, 60);
+            Canvas.SetLeft(txtSerial, 145);
+
+            Rectangle button_rj = new Rectangle(320, 60);
+            button_rj.Fill = new SolidColorBrush(GT.Color.Green); ;//colore di bordo
+            canvas.Children.Add(button_rj);
+            Canvas.SetTop(button_rj, 160);
+            Canvas.SetLeft(button_rj, 0);
+
+            txtSerial = new Text(baseFont, "RJ45");
+            txtSerial.ForeColor = GT.Color.White;
+            canvas.Children.Add(txtSerial);
+            Canvas.SetTop(txtSerial, 180);
+            Canvas.SetLeft(txtSerial, 145);
+        
+        
+        
         }
 
+        
 
 
         private double gasCalibration()
