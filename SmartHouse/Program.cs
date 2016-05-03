0ad6197865ad62e94d6d86ab27726de9e8846352
AleksPolinkins <a.polinkins@gmail.com>
2016-05-03 12:44:27 +0200

    canvas used

--- SmartHouse/Program.cs
+++ SmartHouse/Program.cs
@@ -23,8 +23,14 @@ namespace SmartHouse
     public partial class Program
     {
         //double factor;
+        private Text txtSerial;
+        private Text txtblank;
+  
+        private Canvas canvas;
+      
+        private Font baseFont;
        
-        Window window;
+       
         
         // This method is run when the mainboard is powered up or reset.   
         void ProgramStarted()
@@ -42,14 +48,25 @@ namespace SmartHouse
                 timer.Start();
             *******************************************************************************************/
             //factor = gasCalibration();
-            gasSense.HeatingElementEnabled = true;
-            displayT35.SimpleGraphics.BackgroundColor = GT.Color.Purple;
-            GT.Timer timer = new GT.Timer(500);
-            timer.Tick += my_display;
-           timer.Start();
-           window = displayT35.WPFWindow;
-           window.TouchDown += new Microsoft.SPOT.Input.TouchEventHandler(heating_button);
 
+            //define canvas
+            baseFont = Resources.GetFont(Resources.FontResources.NinaB);
+           canvas = new Canvas();
+           displayT35.WPFWindow.Child = canvas;
+           
+            displayT35.WPFWindow.TouchDown += new Microsoft.SPOT.Input.TouchEventHandler(screen_click);
+            gasSense.HeatingElementEnabled = true;
+          
+           // displayT35.SimpleGraphics.Clear();
+           // displayT35.SimpleGraphics.BackgroundColor = GT.Color.Purple;
+            //var bc = new BrushConverter();
+            
+            //show_states_page();
+           GT.Timer timer = new GT.Timer(500);
+          timer.Tick += my_display;
+           timer.Start();
+
+           display_state_page();
           
             /*
 
@@ -71,11 +88,12 @@ namespace SmartHouse
             Debug.Print("Program Started");
         }
 
-        void heating_button(object sender, Microsoft.SPOT.Input.TouchEventArgs e) {
+        void screen_click(object sender, Microsoft.SPOT.Input.TouchEventArgs e) {
             int x;
             int y;
-            e.GetPosition(window, 0, out x, out y);
+            e.GetPosition(displayT35.WPFWindow, 0, out x, out y);
             if (x >= 259 && x < 340 && y >= 199 && y < 240) {
+                //gas on of button pressed
                 if (gasSense.HeatingElementEnabled)
                 {
                     gasSense.HeatingElementEnabled = false;
@@ -87,25 +105,43 @@ namespace SmartHouse
                
             }
         }
-
+        
         void my_display(GT.Timer timer)
-        {
-            //bmp.Flush();
+        {       
             TempHumidSI70.Measurement temp = tempHumidSI70.TakeMeasurement();
-            //Debug.Print("Temp: " + temp.Temperature.ToString());
-            //Debug.Print("Umidity: " + temp.RelativeHumidity.ToString());
-            
-           
             double gas = gasSense.ReadProportion();
-            String textt = "Temp: " + temp.Temperature.ToString("F2");
-            String textu = "Umidity: " + temp.RelativeHumidity.ToString("F2");
-            String textg = "Gas: " + gas.ToString("F2")+" ( " +gasSense.ReadVoltage().ToString("F2")+" )";
-            Font mfont = Resources.GetFont(Resources.FontResources.NinaB);
-            displayT35.SimpleGraphics.Clear();
-            displayT35.SimpleGraphics.DisplayText(textt, mfont,GT.Color.Cyan, 10, 10);
-            displayT35.SimpleGraphics.DisplayText(textu, mfont, GT.Color.Cyan, 10, 25);
-            displayT35.SimpleGraphics.DisplayText(textg, mfont, GT.Color.Cyan, 10, 40);
-            displayT35.SimpleGraphics.DisplayRectangle(GT.Color.Black, 1, GT.Color.Blue, 259, 199, 60, 40);
+            String textt = temp.Temperature.ToString("F2");
+            String textu = temp.RelativeHumidity.ToString("F2");
+            String textg = gas.ToString("F2")+" (V: " +gasSense.ReadVoltage().ToString("F2")+" )";
+            
+            var window = displayT35.WPFWindow;
+            baseFont = Resources.GetFont(Resources.FontResources.NinaB);
+            //canvas_dyn = new Canvas();
+            //window.Child = canvas_dyn;
+
+
+            String txt = "        ";
+            txtblank = new Text(baseFont, txt);
+            txtSerial = new Text(baseFont, textt);
+            canvas.Children.Add(txtSerial);
+            Canvas.SetTop(txtblank, 40);
+            Canvas.SetLeft(txtblank, 150);
+            Canvas.SetTop(txtSerial, 40);
+            Canvas.SetLeft(txtSerial, 150);
+
+            txtSerial = new Text(baseFont, textu);
+            canvas.Children.Add(txtSerial);
+            Canvas.SetTop(txtSerial, 60);
+            Canvas.SetLeft(txtSerial, 150);
+
+            txtSerial = new Text(baseFont, textg);
+            canvas.Children.Add(txtSerial);
+            Canvas.SetTop(txtSerial, 80);
+            Canvas.SetLeft(txtSerial, 150);
+
+
+            /*
+           
             if (gasSense.HeatingElementEnabled)
             {
                 displayT35.SimpleGraphics.DisplayText("ON", mfont, GT.Color.Red, 284, 213);
@@ -113,12 +149,50 @@ namespace SmartHouse
             else {
                 displayT35.SimpleGraphics.DisplayText("OFF", mfont, GT.Color.Red, 284, 213);
             }
-            
+            */
             
             //Debug.Print("Gas: " + gas);
             
 
         }
+
+        void show_states_page() {
+            Font mfont = Resources.GetFont(Resources.FontResources.NinaB);
+           displayT35.WPFWindow.DisplayModule.SimpleGraphics.DisplayText("Temp: ", mfont, GT.Color.Cyan, 10, 10);
+           displayT35.WPFWindow.DisplayModule.SimpleGraphics.DisplayText("Umidity: ", mfont, GT.Color.Cyan, 10, 25);
+           displayT35.WPFWindow.DisplayModule.SimpleGraphics.DisplayText("Gas: ", mfont, GT.Color.Cyan, 10, 40);
+           displayT35.WPFWindow.DisplayModule.SimpleGraphics.DisplayRectangle(GT.Color.Black, 1, GT.Color.Blue, 259, 199, 60, 40);
+        
+        }
+        private void display_state_page()
+        {
+            
+           
+
+            //
+            // The following displays as intended: (OK)
+            //
+            txtSerial = new Text(baseFont, "Temp: ");
+            canvas.Children.Add(txtSerial);
+            Canvas.SetTop(txtSerial, 40);
+            Canvas.SetLeft(txtSerial, 60);
+
+            // The following displays as: The serial device is...
+            //
+            txtSerial = new Text(baseFont, "Humidity: ");
+            canvas.Children.Add(txtSerial);
+            Canvas.SetTop(txtSerial, 60);
+            Canvas.SetLeft(txtSerial, 60);
+
+            // The following displays as: The serial...
+            //
+            txtSerial = new Text(baseFont, "Gas: ");
+            canvas.Children.Add(txtSerial);
+            Canvas.SetTop(txtSerial, 80);
+            Canvas.SetLeft(txtSerial, 60);
+        }
+
+
 
         private double gasCalibration()
         {
